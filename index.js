00e575a8781c6b9606906d4caff08d0afd67f9dc
nc-hung <nguyenhung11a6@gmail.com>
2022-03-19 21:10:47 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 fetch('https://corona.lmao.ninja/v2/countries/')
     .then(responsive => responsive.json())
-    .then(data => renderDT(data));
-
-
+    .then(data => renderDT(data, content));
 
 let content = document.getElementById('content');
 let tableX = document.createElement('tr');
@@ -31,10 +29,10 @@ tableX.appendChild(th5);
 tableX.appendChild(th6);
 tableX.appendChild(th7);
 
-let renderDT = function(data) {
+let renderDT = function(data, table) {
     for (let i = 0; i < data.length; i++) {
         let tr = document.createElement('tr');
-        content.appendChild(tr);
+        table.appendChild(tr);
 
         let th_1 = document.createElement('td');
         th_1.innerText = data[i].country;
@@ -76,19 +74,60 @@ let renderDT = function(data) {
     }
 }
 
+let searchDT = function(data) {
+    let table = document.getElementById('tb1')
+    let input = document.getElementById('myinput');
+    let btn = document.getElementsByTagName('button');
+    btn[0].onclick = function() {
+        for (let i = 0; i < data.length; i++) {
+            if (input.value == data[i].country || input.value == data[i].countryInfo._id) {
+                let tr = document.createElement('tr');
+                table.appendChild(tr);
 
+                let th_1 = document.createElement('td');
+                th_1.innerText = data[i].country;
+                th_1.style.backgroundColor = '#5dcac3a6';
 
+                let img = document.createElement('img');
+                img.src = data[i].countryInfo.flag;
+                img.style.width = '40px';
+                img.style.float = 'right';
+                th_1.appendChild(img)
+                tr.appendChild(th_1);
 
-//////////////
-let value = document.getElementById('myinput');
-let btn = document.getElementsByTagName('button');
-btn[0].onclick = function(data) {
-    // console.log(value.value)
-    // value.value = "";
-    for (let i = 0; i < data.length; i++) {
-        if (value.value == data[i].country || value.value == data[i].countryInfo._id) {
-            console.log(data[i].country);
+                let th_2 = document.createElement('td');
+                th_2.innerText = data[i].cases;
+                th_2.style.color = '#ff9d00';
+                tr.appendChild(th_2);
 
+                let th_3 = document.createElement('td');
+                th_3.innerText = data[i].todayCases;
+                tr.appendChild(th_3);
+
+                let th_4 = document.createElement('td');
+                th_4.innerText = data[i].deaths;
+                th_4.style.color = '#f64e63';
+                tr.appendChild(th_4);
+
+                let th_5 = document.createElement('td');
+                th_5.innerText = data[i].todayDeaths;
+                tr.appendChild(th_5);
+
+                let th_6 = document.createElement('td');
+                th_6.innerText = data[i].recovered;
+                th_6.style.color = '#65dd9b';
+                tr.appendChild(th_6);
+
+                let th_7 = document.createElement('td');
+                th_7.innerText = data[i].active;
+                tr.appendChild(th_7);
+            }
         }
+        input.value = "";
     }
 }
+
+
+fetch('https://corona.lmao.ninja/v2/countries/')
+    .then(responsive => responsive.json())
+    .then(data => searchDT(data));
